81c87cb9471b1deb5b26f7c8cdadc87a3d5cbdbe
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-17 18:24:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -143,4 +143,12 @@ async function submitRequest(e) {
 // Function to send request to Trello
 function sendToTrello(request) {
     const trelloKey = 'be54e3f7ff2c69550f1ac28b202b7458';
-    const trelloToken = 'ATTAc64ad16d6a5dfa2af0d106b42cb1c9ffad6f80ac4c1e3fce4bb03801473eff247ED
+    const trelloToken = 'ATTAc64ad16d6a5dfa2af0d106b42cb1c9ffad6f80ac4c1e3fce4bb03801473eff247EDCE65C';
+    const listId = '6650d37d314c2a17bbcf7090'; 
+
+    fetch(`https://api.trello.com/1/cards?key=${trelloKey}&token=${trelloToken}&idList=${listId}&name=${encodeURIComponent('New Request')}&desc=${encodeURIComponent(`Type: ${request.shootingType}\nDate: ${request.date}\nAddress: ${request.address}\nCity: ${request.city}\nAdditional Info: ${request.additionalInfo}`)}`, {
+        method: 'POST'
+    }).then(response => response.json())
+      .then(data => console.log('Trello card created:', data))
+      .catch(error => console.error('Error creating Trello card:', error));
+}
