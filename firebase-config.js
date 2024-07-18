73d84ea0bb35faabdd36ce7b22870e88e32d8b4f
VISUARA <98740111+visuarafr@users.noreply.github.com>
2024-07-18 19:05:44 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,24 @@
+// firebase-config.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-storage.js";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBKjWfl5lMhM1cftbrPK2ZbkaBOxqYGp7Y",
+    authDomain: "demande-shooting.firebaseapp.com",
+    projectId: "demande-shooting",
+    storageBucket: "demande-shooting.appspot.com",
+    messagingSenderId: "445564757883",
+    appId: "1:445564757883:web:605f43b554324a6e483fde",
+    measurementId: "G-RCRMBS7X79"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { auth, db, storage };
 
